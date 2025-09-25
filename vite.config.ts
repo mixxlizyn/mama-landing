c072bf8b45b207d35a938534abbc503f97b986bf
mixxlizyn <yelizaveta_baturina@inbox.ru>
2025-09-25 19:47:23 +0500

    ошибки

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/mama-landing/", // добавляем для GitHub Pages (имя репозитория)
   server: {
     host: "::",
     port: 8080,
